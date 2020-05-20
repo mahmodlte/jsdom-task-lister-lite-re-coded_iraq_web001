d9fa1239472cd2644e51fe7f420bed44e2c3e002
Mahmoud Mansour <mahmodlte@gmail.com>
2020-05-20 05:29:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+<<<<<<< HEAD
   const form = document.getElementById("create-task-form");
   form.addEventListener('submit', function(e){
     e.preventDefault();
@@ -13,5 +14,16 @@ document.addEventListener("DOMContentLoaded", () => {
     
    
  
+=======
+  const taskValue = document.getElementById("new-task-description").value;
+  const form = document.getElementById("create-task-form");
+  const listUl = document.getElementById("tasks");
+  const listItem = document.createElement("li");
+  form.addEventListener('submit', function(e){
+   e.preventDefault();
+   listItem.innerText = taskValue;
+   listUl.appendChild(listItem);
+   
+>>>>>>> ba408a461d6b350ba8a14da9ff47f962a41ff8af
   });
 });
