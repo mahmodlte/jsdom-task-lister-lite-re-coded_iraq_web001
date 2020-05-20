9d65d271932a7da624358b484c9c65fc589a7d59
Mahmoud Mansour <mahmodlte@gmail.com>
2020-05-20 08:29:10 +0300

    completed preventDefult lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    let taskValue = document.getElementById("new-task-description").value;
+    const listUl = document.getElementById("tasks");
+    const listItem = document.createElement('li');
+    listUl.appendChild(listItem);
+    console.log(`This is task ${taskValue}`)
+    listItem.innerText = taskValue;
+   
+    
+   
+ 
+  });
 });
